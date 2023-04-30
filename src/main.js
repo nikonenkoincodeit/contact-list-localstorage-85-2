d9322aa571e4d35798264cfbd0d65614cd6f9489
vadimfront <vadimzila94@gmail.com>
2023-04-30 15:28:15 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
+import { formEl } from "./refs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  // let newObj = {};
+  //   formEl.elements.__proto__.forEach = [].forEach;
+  //   formEl.elements.forEach((el) => {
+  //     if (el.nodeName === "INPUT") {
+  //       const { name, value } = el;
+  //       newObj = {
+  //         ...newObj,
+  //         [name]: value,
+  //       };
+  //     }
+  //   });
+  const newObj = Object.fromEntries(new FormData(e.target));
+  newObj.id = Date.now();
+  e.target.reset();
+  console.log(newObj);
+}
+
+formEl.addEventListener("submit", handlerSubmit);
